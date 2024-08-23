061b9d9ae6258503239e7c0329e57e044a945d0a
Techtees <taiwojoshuaoye@gmail.com>
2024-08-23 13:31:13 +0100

    route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,52 @@
-const registerUser = (req, res) => {
-    // check if user has an email
-    if(!req.body.email) {
-        res.status(400)
-        throw new Error("Please add an email")
-    }
-     res.send("Register user")
-};
+const asyncHandler = require("express-async-handler")
+const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
+
+
+const registerUser = asyncHandler(
+    async(req, res) => {
+        const {name, email, password} = req.body
+
+        // validation
+        if(!name || !email || !password) {
+            res.status(400)
+            throw new Error ("Please fill all required fields")
+        }
+
+        if(password.length < 6) {
+            res.status(400)
+            throw new Error ("Password must be up to 6 characters ")
+        }
+
+        // check if user email already exist
+       const userExist = await User.findOne({email})
+
+       if(userExist) {
+         res.status(400)
+         throw new Error ("Email has been used")
+       }
+           
+
+        //    create new user
+        const user = await User.create({
+            name,
+            email,
+            password,
+        })
+
+        if(user) {
+            const {_id, name, email, photo, phone, bio} = user
+            res.status(201).json({
+                _id, name, email, photo, phone, bio
+            })
+        }
+
+        else {
+            res.status(400)
+            throw new Error("Invalid User data")
+        }
+     }
+) 
 
 module.exports =  {
     registerUser
