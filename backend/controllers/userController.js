0dc383f43c85af65110739a8f8af60338c9728fc
Techtees <taiwojoshuaoye@gmail.com>
2024-08-26 14:36:16 +0100

    create login status controller function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -149,6 +149,19 @@ const getUser = asyncHandler(
 
 const loginStatus = asyncHandler(
     async (req, res) => {
+        const token = req.cookies.token;
+
+        if(!token) {
+            return res.json(false)
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(verified) {
+            return res.json(true)
+        }
+        return res.json(false)
+
 
     }
 )
