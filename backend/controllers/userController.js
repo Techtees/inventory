f658afbc0867ee3847fd6a8eda493f7420104a48
Techtees <taiwojoshuaoye@gmail.com>
2024-09-02 17:44:07 +0100

    created route to update user controller function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -168,7 +168,32 @@ const loginStatus = asyncHandler(
 
 const updateUser = asyncHandler(
     async(req, res) => {
-        res.send('user Updated')
+        const user = await User.findById(req.user._id)
+
+        if(user) {
+            const { name, email, photo, phone, bio} = user
+
+            user.email = email;
+            user.name = req.body.name || name;
+            user.phone = req.body.phone || phone;
+            user.bio = req.body.bio || bio;
+            user.photo = req.body.photo || photo;
+
+            const updatedUser = await user.save()
+
+            res.status(200).json({
+                _id: updatedUser._id, 
+                name: updatedUser.name,
+                email: updatedUser.email,
+                photo: updatedUser.photo, 
+                phone: updatedUser.phone,
+                bio: updatedUser.bio,
+            })
+
+        } else {
+            res.status(404)
+            throw new Error("User not found")
+        }
     }
 )
 
