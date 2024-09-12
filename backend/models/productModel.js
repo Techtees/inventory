0e7ad1b56b432aded9ebf0c4aae82c5292af3fca
Techtees <taiwojoshuaoye@gmail.com>
2024-09-12 17:47:47 +0100

    create product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose")
+
+const productSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+   name: {
+        type: String,
+        require: [true, "Please add a name"],
+        trim: true
+    },
+    sku: {
+        type: String,
+        require: true,
+        defalut: "SKU",
+    },
+    category: {
+        type: String,
+        require: [true, "Please add  a category"],
+        trim: true
+    },
+    quantity: {
+        type: String,
+        require: [true, "Please add  a quantity"],
+        trim: true
+    },
+    price: {
+        type: String,
+        require: [true, "Please add  a price"],
+        trim: true
+    },
+    category: {
+        type: String,
+        require: [true, "Please add  a description"],
+        trim: true
+    },
+    image: {
+        type: Object,
+        default: {}
+    },
+}, {
+    timestamps: true
+}
+)
+
+const Product = mongoose.model("Token", productSchema)
+module.exports = Product
