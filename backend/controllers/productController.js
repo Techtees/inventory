09aa878d633e14214269a79053411d6eaaa4042c
Techtees <taiwojoshuaoye@gmail.com>
2024-09-16 19:35:13 +0100

    update product controller function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ const {fileSizeFormatter} = require("../utils/fileUpload")
 const cloudinary = require("cloudinary").v2;
 
 
-
+// create product
 const createProduct = asyncHandler(
     async(req,res) => {
         const {name, sku, category, quantity, price, description} = req.body
@@ -53,7 +53,6 @@ const createProduct = asyncHandler(
 )
 
 // Get all Product
-
 const getProducts = asyncHandler(
     async(req,res) => {
         const products = await Product.find({user: req.user.id}).sort("-createdAt")
@@ -62,7 +61,7 @@ const getProducts = asyncHandler(
     }
 )
 
-// get sinfle product
+// get single product
 const getProduct = asyncHandler(
     async(req, res) =>  {
         const product = await Product.findById(req.params.id)
@@ -84,6 +83,7 @@ const getProduct = asyncHandler(
     }
 )
 
+// delete product
 const deleteProduct = asyncHandler(
     async(req, res) => {
         const product = await Product.findById(req.params.id)
@@ -107,9 +107,78 @@ const deleteProduct = asyncHandler(
     }
 )
 
+const updateProduct = asyncHandler(
+    async(req, res) => {
+        const {name, sku, category, quantity, price, description} = req.body
+
+        const {id} = req.params
+        const product = await Product.findById(id)
+
+        if(!product) {
+            res.status(404)
+            throw new Error("product not found")
+        }
+
+        if(product.user.toString() !== req.user.id){
+            if(!product){
+                res.status(401)
+                throw new Error("User not authorized")
+            }
+        }
+
+
+        
+        // handle image upload
+        let fileData = {}
+        if(req.file) {
+            // save file to cloudinary
+
+            let uploadedFile
+
+            try {
+                uploadedFile = await cloudinary.uploader.upload(req.file.path, {folder: "invent App", resource_type: "image"})
+            } catch (error) {
+                res.status(500)
+                throw new Error("Image could not be uploaded")
+                
+            }
+            fileData = {
+                fileName: req.file.originalname,
+                filePath: uploadedFile.secure_url,
+                fileType: req.file.mimetype,
+                fileSize :fileSizeFormatter(req.file.size, 2)
+            }
+        }
+
+        // Update product
+
+        const updatedProduct = await Product.findByIdAndUpdate(
+            {_id : id},
+            {
+                name,
+                sku,
+                category,
+                quantity,
+                price,
+                description,
+                image: Object.keys(fileData).length === 0 ? product?.image : fileData
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+
+        
+
+        res.status(201).json(updatedProduct)
+    }
+)
+
 module.exports = {
     createProduct,
     getProducts,
     getProduct,
-    deleteProduct
+    deleteProduct,
+    updateProduct
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const protect = require('../middleWare/authMiddleware')
-const { createProduct, getProduct, getProducts, deleteProduct } = require('../controllers/productController')
+const { createProduct, getProduct, getProducts, deleteProduct, updateProduct } = require('../controllers/productController')
 const {upload} = require("../utils/fileUpload")
 const router = express.Router()
 
-router.post("/create", protect, upload.single("image"), createProduct)
+router.post("/", protect, upload.single("image"), createProduct)
 router.get("/", protect, getProducts )
 router.get("/:id", protect, getProduct )
 router.delete("/:id", protect, deleteProduct)
+router.patch("/:id", protect, upload.single("image"), updateProduct)
+
+
 
 
 
